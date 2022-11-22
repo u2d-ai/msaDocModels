3fb5a7923f2c3e2846f780100dcbb9d89fcb5401
vitaliysekach <vitaliy.sekach@gmail.com>
2022-11-22 12:54:02 +0200

    add OpenApiInfo

---
+++ msaDocModels/openapi.py
@@ -0,0 +1,18 @@
+from typing import Optional, List
+
+from pydantic import BaseModel
+
+
+class MSAOpenAPIInfo(BaseModel):
+    """
+    **MSAOpenAPIInfo** Pydantic Response Class
+    """
+
+    name: str = "msaSDK Service"
+    """Service Name."""
+    version: str = "0.0.0"
+    """API Version."""
+    url: str = "/openapi.json"
+    """OpenAPI URL."""
+    tags: Optional[List[str]] = None
+    """OpenAPI Tags."""
