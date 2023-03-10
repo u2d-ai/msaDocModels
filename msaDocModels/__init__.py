099ddcb6f502f889486f7daffeb333a4f1c4d2a1
vitaliysekach <vitaliy.sekach@gmail.com>
2023-03-10 16:26:59 +0200

    fix: change structure for SPKDBBaseDocumentInput

--- msaDocModels/__init__.py
+++ msaDocModels/__init__.py
@@ -1,7 +1,7 @@
 import glob
 from os.path import basename, dirname, isfile, join
 
-version = "0.0.41"
+version = "0.0.42"
 __author__ = "Stefan Welcker"
 __copyright__ = "Copyright 2022, U2D.ai"
 __license__ = "MIT"
